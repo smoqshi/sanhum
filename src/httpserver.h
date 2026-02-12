458894efda3c0ca81f60e161f39dca56ffb2e9ee
smoqshi <47883858+smoqshi@users.noreply.github.com>
2026-02-12 22:17:06 +0300

    Update httpserver.h

--- src/httpserver.h
+++ src/httpserver.h
@@ -47,3 +47,52 @@ private:
 };
 
 #endif // HTTPSERVER_H
+#ifndef HTTPSERVER_H
+#define HTTPSERVER_H
+
+#include <QObject>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QByteArray>
+
+class RobotModel;
+
+#ifdef Q_OS_LINUX
+#include <QProcess>
+#endif
+
+class HttpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit HttpServer(RobotModel *model, QObject *parent = nullptr);
+
+    bool listen(quint16 port);
+
+private slots:
+    void onNewConnection();
+    void onReadyRead();
+    void onDisconnected();
+
+private:
+    void handleRequest(QTcpSocket *socket, const QByteArray &request);
+
+    QTcpServer m_server;
+    RobotModel *m_model;
+
+#ifdef Q_OS_LINUX
+    QProcess m_procCsi;
+    QProcess m_procStereoLeft;
+    QProcess m_procStereoRight;
+#endif
+
+    QByteArray m_csiBuffer;
+    QByteArray m_stereoLeftBuffer;
+    QByteArray m_stereoRightBuffer;
+
+    QByteArray m_lastCsiFrame;
+    QByteArray m_lastStereoLeftFrame;
+    QByteArray m_lastStereoRightFrame;
+};
+
+#endif // HTTPSERVER_H
