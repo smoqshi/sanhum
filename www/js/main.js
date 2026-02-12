db9de18e753ce6930d5d0518dd530ddb4827a322
smoqshi <47883858+smoqshi@users.noreply.github.com>
2026-02-13 00:54:58 +0300

    Update main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,5 @@
 import { tank } from './robotState.js';
-import { initChassis, drawChassis, updateBase } from './chassis.js';
+import { initChassis, drawChassis } from './chassis.js';
 import { initManipulator, drawManipulator } from './manipulator.js';
 import { initUI, updateControls, updateDashboardFromState } from './uiControls.js';
 import { initNetwork, pollStatus } from './network.js';
@@ -10,8 +10,10 @@ const ctx = canvas.getContext('2d');
 initChassis(canvas);
 initManipulator();
 initUI();
-initNetwork();        // ВАЖНО: привязать кнопку и т.п.
-pollStatus();         // один раз запросить статус при старте
+initNetwork();
+
+// один раз запросить статус при старте
+pollStatus();
 
 let lastTime = performance.now();
 
@@ -19,13 +21,12 @@ function loop(time) {
   const dt = (time - lastTime) / 1000.0;
   lastTime = time;
 
-  updateBase(dt);
+  // dt влияет только на внутреннее состояние, визуальный центр остаётся неизменным
   updateControls(dt);
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawChassis(ctx);
   drawManipulator(ctx);
-
   updateDashboardFromState();
 
   requestAnimationFrame(loop);
