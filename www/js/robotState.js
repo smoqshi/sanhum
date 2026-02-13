03b28815a6d3e233d85318f990c2cb224dbc7fd6
smoqshi <47883858+smoqshi@users.noreply.github.com>
2026-02-14 00:53:01 +0300

    Update robotState.js

--- www/js/robotState.js
+++ www/js/robotState.js
@@ -1,35 +1,24 @@
-// Нормализованное состояние робота/манипулятора для симуляции и реального робота
 export const tank = {
-    // команды оператора
-    vLinearCmd: 0.0,        // м/с
-    vAngularCmdDeg: 0.0,    // град/с
+    x: 0,
+    y: 0,
+    angle: 0,
+    vLinear: 0,
+    vAngular: 0,
 
-    // фактическое состояние (после интеграции)
-    vLinear: 0.0,
-    vAngularDeg: 0.0,
-    x: 0.0,
-    y: 0.0,
-    yawDeg: 0.0,
+    emergency: false,
+    parkingBrake: false,
 
-    // манипулятор
-    turretAngle: 0.0,       // градусы
-    armExtension: 0.5,      // 0..1
-    gripper: 0.3,           // 0..1
+    battery: 0,
+    cpuTemp: 0,
+    boardTemp: 0,
+    cpuLoad: 0,
+    wifiSsid: '',
+    wifiRssi: 0,
 
-    // режимы
-    simulationMode: true,
-    gamepadConnected: false,
-
-    resetPose() {
-        this.x = 0.0;
-        this.y = 0.0;
-        this.yawDeg = 0.0;
-        this.vLinear = 0.0;
-        this.vAngularDeg = 0.0;
-        this.vLinearCmd = 0.0;
-        this.vAngularCmdDeg = 0.0;
-        this.turretAngle = 0.0;
-        this.armExtension = 0.5;
-        this.gripper = 0.3;
-    }
+    currentTotal: 0,
+    current5V: 0,
+    current12V: 0,
+    currentDriver: 0,
+    currentGpio: 0,
 };
+
