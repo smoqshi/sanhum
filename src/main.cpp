13d467b911d158c25343120ab74b327d62a4da48
smoqshi <47883858+smoqshi@users.noreply.github.com>
2026-02-25 23:43:26 +0300

    Raspberry fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,31 @@
-#include <QApplication>
 #include <rclcpp/rclcpp.hpp>
+
+#ifdef PLATFORM_WINDOWS
+#include <QApplication>
 #include "main_window.h"
+#endif
 
 int main(int argc, char *argv[])
 {
-    // Инициализация ROS2
     rclcpp::init(argc, argv);
 
-    // Инициализация Qt
+#ifdef PLATFORM_WINDOWS
     QApplication app(argc, argv);
-
-    auto ros_node = std::make_shared<rclcpp::Node>("sanhum_qt_node");
-    MainWindow w(ros_node);
+    auto node = std::make_shared<rclcpp::Node>("sanhum_gui");
+    MainWindow w(node);
     w.show();
-
+    auto spin_thread = std::thread([node]() { rclcpp::spin(node); });
     int ret = app.exec();
-
     rclcpp::shutdown();
+    spin_thread.join();
     return ret;
+#else
+    auto node = std::make_shared<rclcpp::Node>("sanhum_robot");
+
+    // здесь запускаешь headless‑логику:
+    // MotorDriver, Esp32Driver, Cameras, YoloDetector, Behaviour и т.п.
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+#endif
 }
