121ded9fdc9088f5b62ba596c7729d718ea70db7
smoqshi <47883858+smoqshi@users.noreply.github.com>
2026-02-26 22:31:43 +0300

    Update main_window.h

--- include/main_window.h
+++ include/main_window.h
@@ -1,82 +1,28 @@
-#ifndef MAIN_WINDOW_H
-#define MAIN_WINDOW_H
+std::shared_ptr<rclcpp::Node> ros_node_;
 
-#include <QMainWindow>
-#include <QPushButton>
-#include <QTimer>
-#include <QTcpSocket>
-#include <memory>
-#include <array>
+RobotViewWidget *robot_view_;
+JoystickWidget *joystick_drive_;
+JoystickWidget *joystick_manip_;
+QPushButton *reset_button_;
 
-#include <rclcpp/rclcpp.hpp>
+QTcpSocket *robot_socket_;
+bool robot_connected_;
+QString robot_host_;
 
-class RobotViewWidget;
-class JoystickWidget;
+double sim_x_;
+double sim_y_;
+double sim_theta_;
+int sim_dt_ms_;
+QTimer *sim_timer_;
 
-// Абстрактное состояние управления от геймпада/клавиатуры
-struct ControlState
-{
-    double drive_v;        // -1..1, движение вперёд/назад (левый стик Y / W,S)
-    double drive_w;        // -1..1, поворот (правый стик X / A,D)
-    double manip_extend;   // -1..1, удлинение манипулятора (правый стик Y / I,K)
-    double manip_height;   // -1..1, высота манипулятора (LT / LT+LB / U,J)
-    bool   grip_closed;    // захват закрыт (RT / O)
-};
+GamepadControl *gamepad_;
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+struct ControlState {
+    double drive_v;
+    double drive_w;
+    double manip_extend;
+    double manip_height;
+    bool   grip_closed;
+} control_;
 
-public:
-    explicit MainWindow(std::shared_ptr<rclcpp::Node> node,
-                        QWidget *parent = nullptr);
-
-    // Задать IP Raspberry Pi; пустая строка -> чистая симуляция
-    void setRobotHost(const QString &ip);
-
-protected:
-    void keyPressEvent(QKeyEvent *event) override;
-    void keyReleaseEvent(QKeyEvent *event) override;
-
-private slots:
-    void onSimUpdate();
-    void onResetPose();
-    void onRobotConnected();
-    void onRobotDisconnected();
-
-private:
-    void sendDriveAndManipulatorCommand(double u_L, double u_R);
-    void updateManipulatorModel(double dt);
-
-private:
-    // ROS2
-    std::shared_ptr<rclcpp::Node> ros_node_;
-
-    // Виджеты
-    RobotViewWidget *robot_view_;
-    JoystickWidget  *joystick_drive_;
-    JoystickWidget  *joystick_manip_;
-    QPushButton     *reset_button_;
-
-    // Сетевое соединение с Raspberry Pi
-    QTcpSocket *robot_socket_;
-    bool        robot_connected_;
-    QString     robot_host_;
-
-    // Таймер симуляции
-    QTimer *sim_timer_;
-    int     sim_dt_ms_;
-
-    // Состояние симуляции гусеничного шасси
-    double sim_x_;      // м
-    double sim_y_;      // м
-    double sim_theta_;  // рад
-
-    // Состояние манипулятора (4 звена)
-    std::array<double, 4> joints_;
-
-    // Абстрактное состояние управления (геймпад + клавиатура)
-    ControlState control_;
-};
-
-#endif // MAIN_WINDOW_H
+std::array<double, 4> joints_;
