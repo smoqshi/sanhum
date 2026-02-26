78ab3ef16e9137488ab4a5bb21de5d56ae708e5a
smoqshi <47883858+smoqshi@users.noreply.github.com>
2026-02-26 22:32:29 +0300

    Update main_window.h

--- include/main_window.h
+++ include/main_window.h
@@ -1,28 +1,68 @@
-std::shared_ptr<rclcpp::Node> ros_node_;
+#pragma once
 
-RobotViewWidget *robot_view_;
-JoystickWidget *joystick_drive_;
-JoystickWidget *joystick_manip_;
-QPushButton *reset_button_;
+#include <QMainWindow>
+#include <QPushButton>
+#include <QTcpSocket>
+#include <QTimer>
+#include <array>
+#include <memory>
 
-QTcpSocket *robot_socket_;
-bool robot_connected_;
-QString robot_host_;
+#include <rclcpp/rclcpp.hpp>
 
-double sim_x_;
-double sim_y_;
-double sim_theta_;
-int sim_dt_ms_;
-QTimer *sim_timer_;
+class RobotViewWidget;
+class JoystickWidget;
+class GamepadControl;
 
-GamepadControl *gamepad_;
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit MainWindow(std::shared_ptr<rclcpp::Node> node,
+                        QWidget *parent = nullptr);
 
-struct ControlState {
-    double drive_v;
-    double drive_w;
-    double manip_extend;
-    double manip_height;
-    bool   grip_closed;
-} control_;
+    void setRobotHost(const QString &ip);
 
-std::array<double, 4> joints_;
+protected:
+    void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+
+private slots:
+    void onRobotConnected();
+    void onRobotDisconnected();
+    void onResetPose();
+    void onSimUpdate();
+
+private:
+    void updateManipulatorModel(double dt);
+    void sendDriveAndManipulatorCommand(double u_L, double u_R);
+
+    struct ControlState {
+        double drive_v;        // -1..1, линейная скорость шасси
+        double drive_w;        // -1..1, поворот
+        double manip_extend;   // -1..1, удлинение манипулятора
+        double manip_height;   // -1..1, высота манипулятора
+        bool   grip_closed;    // состояние захвата
+    };
+
+    std::shared_ptr<rclcpp::Node> ros_node_;
+
+    RobotViewWidget *robot_view_;
+    JoystickWidget *joystick_drive_;
+    JoystickWidget *joystick_manip_;
+    QPushButton *reset_button_;
+
+    QTcpSocket *robot_socket_;
+    bool robot_connected_;
+    QString robot_host_;
+
+    double sim_x_;
+    double sim_y_;
+    double sim_theta_;
+    int    sim_dt_ms_;
+    QTimer *sim_timer_;
+
+    GamepadControl *gamepad_;
+
+    ControlState control_;
+    std::array<double, 4> joints_;
+};
